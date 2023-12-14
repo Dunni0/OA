06edade362a0cb2623b3f5e8c8090b246979807f
Dunni <pelumioladunni3@gmail.com>
2023-12-14 16:34:30 +0100

    ...

--- portfolio.js
+++ portfolio.js
@@ -17,3 +17,61 @@ const linkedin = document.querySelector("#linkedin")
 linkedin.addEventListener("click", () =>{
      location.href="https://www.linkedin.com/in/oluwapelumi-awonuga-841997221/"
 })
+
+const cv = document.querySelector("#cv")
+cv.addEventListener("click", () => {
+     window.open('./assets/My Resume (intern).pdf')
+})
+
+const github = document.querySelector("#github")
+
+github.addEventListener("click", () => {
+     location.href="https://github.com/Dunni0"
+})
+
+const recipe_git = document.querySelector("#recipe_git")
+
+recipe_git.addEventListener("click", () => {
+     location.href="https://github.com/Dunni0/Recipe-app"
+})
+
+const recipe_live = document.querySelector("#recipe_live")
+
+recipe_live.addEventListener("click", () => {
+     location.href="https://recipe-app-dunni0.vercel.app/"
+})
+
+const reading_git = document.querySelector("#reading_git")
+
+reading_git.addEventListener("click", () => {
+     location.href="https://github.com/Dunni0/reading-tracker"
+})
+
+const reading_live = document.querySelector("#reading_live")
+
+reading_live.addEventListener("click", () => {
+     location.href="https://recipe-app-dunni0.vercel.app/"
+})
+
+const crop_git = document.querySelector("#crop_git")
+
+crop_git.addEventListener("click", () => {
+     location.href="https://github.com/Dunni0/Crop-Pro"
+})
+
+const crop_live = document.querySelector("#crop_live")
+
+crop_live.addEventListener("click", () => {
+     location.href="https://github.com/Dunni0/Crop-Pro"
+})
+
+const mail = document.querySelector("#mail")
+
+mail.addEventListener("click", () => {
+     location.href='mailto:pelumioladunni3@gmail.com'
+})
+
+const linkedin2 = document.querySelector("#linkedin2")
+linkedin2.addEventListener("click", () =>{
+     location.href="https://www.linkedin.com/in/oluwapelumi-awonuga-841997221/"
+})
