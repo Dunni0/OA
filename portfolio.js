3a59572734e0db8f39c292d4ed8f9ccd9945f441
Dunni <pelumioladunni3@gmail.com>
2023-12-14 15:36:11 +0100

    ..

--- portfolio.js
+++ portfolio.js
@@ -12,3 +12,8 @@ function toggleMenu(){
      hide.classList.toggle('open')
      console.log("open")
 }
+
+const linkedin = document.querySelector("#linkedin")
+linkedin.addEventListener("click", () =>{
+     location.href="https://www.linkedin.com/in/oluwapelumi-awonuga-841997221/"
+})
